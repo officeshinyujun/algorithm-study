c14751880ad8a8bea297123886cdf1c6ae7d3e3a
officeshinyujun <officeshinyujun.com>
2023-12-29 21:16:05 +0900

    01-2내용정리

---
+++ sum_verbose1.py
@@ -0,0 +1,17 @@
+print('1부터 n까지의 정수의 합을 구합니다.')
+a = int(input('정수 a를 입력하세요.:'))
+b = int(input('정수 b를 입력하세요.:'))
+
+if a>b:
+	a,b = b,a
+sum = 0
+for i in range(a, b+1):
+	if i < b:
+		print (f'{i} + ', end ='')
+	else:
+		print (f'{i} = ', end= '')
+	sum += i
+
+print(sum)
+
+
